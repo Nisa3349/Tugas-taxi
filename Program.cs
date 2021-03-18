96d5f0ac4bf090ea2d308230125b173af2a5f55b
Nisa Alfiani <nisa_alfiani@students.amikom.ac.id>
2021-03-18 12:57:45 +0700

    Praktikum taxi

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugas_taxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //membuat objek taxi
+            Taxi taxi = new Taxi();
+
+            //pengesetan nilai properties
+            taxi.DriveName = "Jono";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            //pemanggilan method
+            taxi.TaxiInfo();
+            taxi.PicUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Taxi.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugas_taxi
+{
+    class Taxi
+    {        //properties
+        public string DriveName { get; set; }
+        public bool OnDuty { get; set; }
+        public float NumPassenger { get; set; }
+
+        //method
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name \t\t: {0}", DriveName);
+            Console.WriteLine("On Duty \t\t: {0}", OnDuty);
+            Console.WriteLine("Number of Passenger \t: {0}\n", NumPassenger);
+            
+        }
+
+        public void PicUpPassenger()
+        {
+             Console.WriteLine("{0} sedang menjemput penumang", DriveName);
+            
+        }
+
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} selesai menjemput penumpang", DriveName);
+        }
+    }
+}
